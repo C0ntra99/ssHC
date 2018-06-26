6cb285e1b2bea908097b3d65d901682ac976657b
root <root@localhost.localdomain>
2018-06-25 20:16:41 -0500

    added more arguments, added TMUX sessions, fixed hashcat, added key auth, added checking for running sessions, reading the cracked hashes

--- ssHC.py
+++ ssHC.py
@@ -3,14 +3,15 @@
 Create unique id for each hash that needs to be cracked x
 write to a file x
 pass the server that file x
-launch hashcat under a screen session x
-check if the screen session is still running
+launch hashcat under a tmux session
+check if the tmux session is still running
 read the cracked hashes
 add argument to handle the hash type
 check for errors while executing command
 add rule file x
 add verbose mode
 write install script
+specify hashcat folder? default /usr/bin
 '''
 import paramiko ##need in requirments
 import getpass
@@ -26,32 +27,50 @@ import itertools
 def parse_args():
     parser = argparse.ArgumentParser(description='Remote hashcat cracker with though ssh')
     parser.add_argument('target', help='[username[:password]@<crackerIP>] if a username or password is not passed, it will prompt', action='store')
-    parser.add_argument('-f', '--hash-file', help='Path to the hashes', required=True, default='D:\GitHub\ssHC\hashes.txt')
-    parser.add_argument('-w', '--wordlist', help='Path to the wordlist on the cracker box', required=False, default='/usr/share/wordlists/rockyou.txt')
-    parser.add_argument('-r', '--rule-file', help='Path to the rule file on the cracker box', required=False, default='/usr/share/hashcat/rules/best64.rule')
-    parser.add_argument('-v', '--verbose', help='Will show the output of hashcat throughout cracking', default=False, action='store_true')
-    return parser.parse_args()
+    parser.add_argument('-k', '--key', help='SSH key for authentication')
+    parser.add_argument('-f', '--hash-file', help='Path to the hashes')#, default='D:\GitHub\ssHC\hashes.txt')
+    parser.add_argument('-w', '--wordlist', help='Path to the wordlist on the cracker box', default='/usr/share/wordlists/rockyou.txt')
+    parser.add_argument('-r', '--rule-file', help='Path to the rule file on the cracker box', default='/usr/share/hashcat/rules/best64.rule')
+    parser.add_argument('-v', '--verbose', help="Only use for debugging, this will not run the hashcat session in a tmux session.", action='store_true')
+    parser.add_argument('-n', '--name', help='Add a custom tmux/hashcat session name', action='store')
+    parser.add_argument('-c', '--check', help='Check to see if a session is still running', action='store')
+    args = parser.parse_args()
+    if args.check and not args.hash_file:
+        return args
+    elif args.hash_file and not args.check:
+        return args
+    elif not args.hash_file and not args.check:
+        parser.print_help()
+        sys.exit("[!]Please use either -f or -c when running")
+    return args
 
-def ssh_client(host, port, user, pw):
+def ssh_client(host, port, user, pw, key=None):
 
     client = paramiko.SSHClient()
     client.load_system_host_keys()
     client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
-    try:
-        client.connect(host, port, user, pw)
-    except:
-        sys.exit('[!]Authentication Failed')
+    ##DOUBLE CHECK THIS
+    if key:
+        try:
+            client.connect(host, port, user, pw, key)
+        except:
+            sys.exit('[!]Authentication Failed')
+    else:
+        try:
+            client.connect(host, port, user, pw)
+        except:
+            sys.exit('[!]Authentication Failed')
 
     return client
 
 def cracker_creds(uname=False, passwd=False, **kwargs):
     if uname and passwd:
         username = input('[*] Crckerbox username: ')
-        pw = pw = getpass.getpass('[*] Crackerbox password: ')
+        pw = pw = getpass.getpass('[*]Crackerbox password: ')
         return username, pw
     if passwd:
-        pw = getpass.getpass('[*] Crackerbox password: ')
+        pw = getpass.getpass('[*]Crackerbox password: ')
         return pw
 
 
@@ -60,18 +79,18 @@ def generate_cmd(id, hash_file, wordlist, rule_file, verbose=False):
     command += ' -m {} '
     command += '-o /tmp/{} /tmp/{} {} -r {} --force'.format(id, hash_file, wordlist, rule_file)
     command = command.format('1000')
+
     if verbose:
         return command
+    tmux = 'tmux new -d -s "{}" {}'. format(id, command)
+    return tmux
 
-    screen = 'screen -S {} -dm {}'. format(id, command)
-    return screen
-
-##Print out the exact output of hashcat
 def runVerbose(id, args):
-    print("Running verbose")
+    print("[*]Verbose mode:")
     stdin, stdout, stderr = cracker_client.exec_command(generate_cmd(id, (id+'.hash'),args.wordlist, args.rule_file, True))
 
-    print('stdout: ', stdout.read().decode())
+    print('[*]Stdout: ',stdout.read().decode())
+    print('[*]Stderr: ', stderr.read().decode())
 
 def main(args):
     if '@' in args.target:
@@ -91,39 +110,58 @@ def main(args):
     cracker_client = ssh_client(target, 22, c_user, c_pw)
     cracker_scp = SCPClient(cracker_client.get_transport())
 
-    rand = ''.join(random.choices(string.ascii_uppercase + string.digits, k=5))
-    id = 'ssHC-' + rand
-
-    if 'nt' in os.name:
-        cracker_scp.put(os.getcwd()+'\\'+args.hash_file,'/tmp/'+id+'.hash')
-    elif 'posix' in os.name:
-        cracker_scp.put(os.getcwd()+'/'+args.hash_file,'/tmp/'+id+'.hash')
-
-
-    print("[+]Cracking started")
-    print('[*]Id is: '+ id)
-
-    if args.verbose:
-        runVerbose(id, args)
+    if args.name:
+        id = args.name
     else:
-        stdin, stdout, stderr = cracker_client.exec_command(generate_cmd(id, (id+'.hash'),args.wordlist, args.rule_file))
+        rand = ''.join(random.choices(string.ascii_uppercase + string.digits, k=5))
+        id = 'ssHC-' + rand
 
-    ##while screen session stiil exists print spinning bar
-    while True:
-        print(cracker_client.exec_command('screen -ls')[1].read().decode())
-        if id in cracker_client.exec_command('screen -ls')[1].read().decode():
-            print(next(spinner), end = '\r')
+    if args.check:
+        id = args.check
+        if args.check in cracker_client.exec_command('tmux ls')[1].read().decode():
+            print("[*]Session {} is currently running".format(args.check))
         else:
-            break
-    ##once the program finishs print out the output
-    __, stdout, stderr = cracker_client.exec_command('cat /tmp/'+id)
-    print('{}'.format(stdout.read().decode()), end='\r')
-    #time.sleep(3)
+            __, stdout, stderr = cracker_client.exec_command('cat /tmp/'+ id)
+            if not stdout.read().decode():
+                print("[+]Session did not exists")
+            else:
+                print("[+]Passwords for session {}".format(id))
+                print("{}".format(stdout.read().decode()))
+    else:
+
+        if 'nt' in os.name:
+            cracker_scp.put(os.getcwd()+'\\'+args.hash_file,'/tmp/'+id+'.hash')
+        elif 'posix' in os.name:
+            cracker_scp.put(os.getcwd()+'/'+args.hash_file,'/tmp/'+id+'.hash')
+
+        print('[*]Id is: '+ id)
+
+        if args.verbose:
+            runVerbose(id, args)
+        else:
+            stdin, stdout, stderr = cracker_client.exec_command(generate_cmd(id, (id+'.hash'),args.wordlist, args.rule_file))
+
+            ##Watch this pls
+            if stderr.read().decode():
+                print("[!]Error, please run again with -v")
+
+        while True:
+            if id in cracker_client.exec_command('tmux ls')[1].read().decode():
+                print("[*]Cracking hashes ",next(spinner), end = '\r')
+            else:
+                break
+            time.sleep(.5)
+        ##once the program finishs print out the output
+        print('[+]Cracked hashes: \n')
+        __, stdout, stderr = cracker_client.exec_command('cat /tmp/'+id)
+        print('{}'.format(stdout.read().decode()), end='\r')
+
 
 if __name__ == "__main__":
     spinner = itertools.cycle(['-', '/', '|', '\\'])
+    args = parse_args()
     try:
-        main(parse_args())
+        main(args)
     except KeyboardInterrupt:
         print('[!]Exiting now')
         sys.exit()
